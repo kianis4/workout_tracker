a54ceb00a0f5fe3e1cccdceda8ac5432d35082c4
Suleyman <suley.kiani@gmail.com>
2024-04-20 02:20:52 -0400

    Added functionality to populate database

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:workout_tracker/auth/auth_service.dart';
 import 'package:workout_tracker/auth/login_screen.dart';
 import 'package:workout_tracker/calculator_screen.dart';
@@ -5,10 +6,39 @@ import 'package:workout_tracker/workouts_screen.dart';
 
 import 'package:workout_tracker/widgets/button.dart';
 import 'package:flutter/material.dart';
+import 'package:csv/csv.dart';
+import 'package:flutter/services.dart' show rootBundle;
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
+  void exportData() async{
+    final CollectionReference exercises=FirebaseFirestore.instance.collection('exercises');
+
+    final myData=await rootBundle.loadString("assets/datasets/exercises.csv");
+    List<List<dynamic>> csvTable=CsvToListConverter().convert(myData);
+    List<List<dynamic>> data=[];
+
+    data=csvTable;
+
+    for(var i=0; i < data.length; i++){
+      var record = {
+        "ExerciseName": data[i][0],
+        "DescriptionURL": data[i][1],
+        "ExerciseImage": data[i][1],
+        "ExerciseImagAlt": data[i][2],
+        "muscleGpDetails": data[i][4],
+        "muscleGp": data[i][5],
+        "equipmentDetails": data[i][6],
+        "Equipment": data[i][7],
+        "Rating": data[i][8],
+        "Description": data[i][9]
+      };
+     exercises.add(record);
+    }
+
+
+  }
   @override
   Widget build(BuildContext context) {
     final auth = AuthService();
@@ -36,6 +66,9 @@ class HomeScreen extends StatelessWidget {
                 goToCalculator(context);
               },
             ),
+            ElevatedButton(onPressed: exportData,
+                child: Text('Export CSV'))
+
             // CustomButton(
             //   label: "Exercise Viewer",
             //   onPressed: () async{
